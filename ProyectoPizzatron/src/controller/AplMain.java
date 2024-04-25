2a252bdcbb735cd9990aad96fd8b243ef21edd95
MariOvallos <167836177+MariOvallos@users.noreply.github.com>
2024-04-25 17:22:02 -0500

    COMENTARIO MARI

--- ProyectoPizzatron/src/controller/AplMain.java
+++ ProyectoPizzatron/src/controller/AplMain.java
@@ -5,7 +5,7 @@ public class AplMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-		//holaaaaa mundo joan
+		//holaaaaa mundo joan MARI
 	}
 
 }
