0f49efe8981aa0387468f5c9a741754f9c9b988f
MariOvallos <167836177+MariOvallos@users.noreply.github.com>
2024-04-28 17:40:49 -0500

    Avance :)

--- ProyectoPizzatron/src/view/VentanaPrincipal.java
+++ ProyectoPizzatron/src/view/VentanaPrincipal.java
@@ -25,6 +25,48 @@ public class VentanaPrincipal extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 
+		setTitle("");
+		setSize(800, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setLayout(null);
+
+		pPanel = new Panel();
+		pPanel.setBounds(10, 10, 760, 540);
+		add(pPanel);
+
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+		setTitle("");
+		setSize(800, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setLayout(null);
+
+		pPanel = new Panel();
+		pPanel.setBounds(100, 70, 600, 400);
+		add(pPanel);
+
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+	}
+
+	public Panel getpTutorial() {
+		return pPanel;
+	}
+
+	public void setpTutorial(Panel pPanel) {
+		this.pPanel = pPanel;
+	}
+
+	public Panel getpBotones() {
+		return pPanel;
+	}
+
+	public void setpBotones(Panel pBotones) {
+		this.pPanel = pBotones;
 	}
 
 	public Panel getpPanel() {
