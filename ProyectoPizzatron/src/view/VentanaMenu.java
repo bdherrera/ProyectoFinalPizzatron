180aa7da5a51e98ef7767045472196247659eaf0
MariOvallos <167836177+MariOvallos@users.noreply.github.com>
2024-05-14 23:45:25 -0500

    :)

--- ProyectoPizzatron/src/view/VentanaMenu.java
+++ ProyectoPizzatron/src/view/VentanaMenu.java
@@ -1,15 +1,12 @@
 package view;
 
 import javax.swing.BorderFactory;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import model.PerfilJugador;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -22,12 +19,10 @@ public class VentanaMenu extends JFrame {
 	private VentanaRegistroJugador jugador3;
 	private VentanaTutorial jugador4;
 	private JButton botonPLAY;
-
 	private JButton botonTutorial;
-
 	private JButton botonProfiles;
 	private JButton botonCharacter;
-	private PerfilJugador perfilJugador; 
+	private PerfilJugador perfilJugador;
 
 	public VentanaMenu(PerfilJugador perfilJugador) {
 		this.perfilJugador = perfilJugador;
@@ -38,53 +33,55 @@ public class VentanaMenu extends JFrame {
 		setLocationRelativeTo(null);
 
 		JPanel panelMenu = new JPanel();
-		panelMenu.setLayout(null); // Usar null layout para control manual
+		panelMenu.setLayout(null);
 		panelMenu.setBackground(new Color(230, 230, 250));
 		panelMenu.setBorder(BorderFactory.createTitledBorder("Menu"));
-		panelMenu.setBounds(0, 0, 830, 635); // Tamaño del panel ajustado al tamaño de la ventana
+		panelMenu.setBounds(0, 0, 830, 635);
 		add(panelMenu);
 
-		ImageIcon imagenIcon = new ImageIcon("media/Botones/Main Menu.png");
-		JLabel imagenLabel = new JLabel(imagenIcon);
-		imagenLabel.setBounds(1, 1, 800, 600); // Tamaño de la imagen ajustado al tamaño de la ventana
-		panelMenu.add(imagenLabel);
+	
+
 
 		ImageIcon playIcon = new ImageIcon("media/Botones/Play Button.png");
 		botonPLAY = new JButton(playIcon);
-		botonPLAY.setBounds(60, 280, 200, 50); // x, y, width, height
-		botonPLAY.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		botonPLAY.setBounds(70, 290, 180, 35);
+		botonPLAY.setOpaque(false); // Establece el fondo del botón como transparente
+		botonPLAY.setContentAreaFilled(false); // Establece el área de contenido del botón como transparente
+		botonPLAY.setBorderPainted(false);
 		botonPLAY.setActionCommand("PLAY");
-		botonPLAY.repaint();
 		panelMenu.add(botonPLAY);
 		botonPLAY.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				jugador = new VentanaPLAY();
 				jugador.setVisible(true);
-				dispose();
+
 			}
 		});
 
 		ImageIcon profilesIcon = new ImageIcon("media/Botones/profiles.png");
 		botonProfiles = new JButton(profilesIcon);
-		botonProfiles.setBounds(60, 320, 200, 50);
-		botonProfiles.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		botonProfiles.setBounds(70, 330, 180, 35);
+		botonProfiles.setOpaque(false); // Establece el fondo del botón como transparente
+		botonProfiles.setContentAreaFilled(false); // Establece el área de contenido del botón como transparente
+		botonProfiles.setBorderPainted(false);
 		botonProfiles.setActionCommand("PROFILES");
-		botonProfiles.repaint();
 		panelMenu.add(botonProfiles);
 		botonProfiles.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				jugador2 = new VentanaProfiles();
 				jugador2.setVisible(true);
-				dispose();
+
 			}
 		});
 
 		ImageIcon tutorialIcon = new ImageIcon("media/Botones/how to play.png");
 		botonTutorial = new JButton(tutorialIcon);
-		botonTutorial.setBounds(60, 370, 200, 50);
-		botonTutorial.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		botonTutorial.setBounds(70, 370, 180, 35); 
+		botonTutorial.setOpaque(false); // Establece el fondo del botón como transparente
+		botonTutorial.setContentAreaFilled(false); // Establece el área de contenido del botón como transparente
+		botonTutorial.setBorderPainted(false);// Ajusta las coordenadas según sea necesario
 		botonTutorial.setActionCommand("TUTORIAL");
 		panelMenu.add(botonTutorial);
 		botonTutorial.addActionListener(new ActionListener() {
@@ -92,26 +89,32 @@ public class VentanaMenu extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				jugador4 = new VentanaTutorial();
 				jugador4.setVisible(true);
-				dispose();
+
 			}
 		});
 
 		ImageIcon characterIcon = new ImageIcon("media/Botones/character.png");
 		botonCharacter = new JButton(characterIcon);
-		botonCharacter.setBounds(60, 410, 200, 50);
-		botonCharacter.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		botonCharacter.setBounds(70, 410, 180, 35);
+		botonCharacter.setOpaque(false); // Establece el fondo del botón como transparente
+		botonCharacter.setContentAreaFilled(false); // Establece el área de contenido del botón como transparente
+		botonCharacter.setBorderPainted(false);
 		botonCharacter.setActionCommand("CHARACTER");
-		botonCharacter.repaint();
 		panelMenu.add(botonCharacter);
 		botonCharacter.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				jugador3 = new VentanaRegistroJugador();
 				jugador3.setVisible(true);
-				dispose();
+
 			}
 		});
-
+		
+		ImageIcon imagenIcon = new ImageIcon("media/Botones/Main Menu.png");
+		JLabel imagenLabel = new JLabel(imagenIcon);
+		imagenLabel.setBounds(1, 1, 800, 600); 
+		panelMenu.add(imagenLabel);
+		
 		JPanel panelContenedor = new JPanel(new BorderLayout());
 		panelContenedor.add(panelMenu, BorderLayout.CENTER);
 
